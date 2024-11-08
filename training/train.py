c9312ed6fd43949ca2ebf759a4469e2690bc0870
nph4rd <arturomf94@gmail.com>
2024-11-08 18:15:58 +0000

    use lower seqlen

--- training/train.py
+++ training/train.py
@@ -99,7 +99,7 @@ def process_batch(
             images=images,
             image_idx=image_idx,
             tokens=np.asarray(tokens).astype(np.int32),
-            sequence_length=1536,
+            sequence_length=64, # we can use lower sequence length here, compared to https://huggingface.co/allenai/Molmo-7B-D-0924/blob/1721478b71306fb7dc671176d5c204dc7a4d27d7/preprocessing_molmo.py#L77
             image_patch_token_id=processor.special_token_ids["<im_patch>"],
             image_col_token_id=processor.special_token_ids["<im_col>"],
             image_start_token_id=processor.special_token_ids["<im_start>"],
