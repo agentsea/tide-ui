530ab475e3a87f8441b95b467a9dac26169252ae
nph4rd <arturomf94@gmail.com>
2024-12-11 23:55:03 +0000

    add inference example for moondream

--- inference/moondream/inference.py
+++ inference/moondream/inference.py
@@ -1,13 +1,27 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
-from PIL import Image
+from PIL import Image, ImageDraw
 import requests
+import torch
 
 model_id = "vikhyatk/moondream-next"
 
 # load model and tokenizer
-model = AutoModelForCausalLM.from_pretrained(model_id, trust_remote_code=True)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    trust_remote_code=True,
+    attn_implementation="flash_attention_2",
+    torch_dtype=torch.bfloat16,
+).to("cuda")
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 image = Image.open(requests.get("https://picsum.photos/id/237/536/354", stream=True).raw)
-# enc_image = model.encode_image(image) # TODO: maybe remove? 
-print(model.point(image, "dog", tokenizer))
+image = Image.open("airbnb_test_input.png")
+points = model.point(image, "search button", tokenizer)
+print(f"points: {points}")
+# Draw point on image
+draw = ImageDraw.Draw(image)
+x = points[0]["x"] * image.width
+y = points[0]["y"] * image.height
+radius = 5
+draw.ellipse([x-radius, y-radius, x+radius, y+radius], fill='red')
+image.save('airbnb_test_output.png')
